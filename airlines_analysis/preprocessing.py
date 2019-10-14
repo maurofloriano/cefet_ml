29e6d044d4ba8ce89f2f5c19fc1f6a3f7a9c2242
Mauro Floriano <maurofloriano@iheartmedia.com>
2019-10-14 07:41:15 -0300

    update on notebook

--- airlines_analysis/preprocessing.py
+++ airlines_analysis/preprocessing.py
@@ -1,6 +1,9 @@
 import re
 from bs4 import BeautifulSoup
 from airlines_analysis.enums import SentimentEnum
+from sklearn.model_selection import train_test_split, StratifiedKFold, cross_val_score
+from nltk.corpus import stopwords
+
 
 class PreprocessingMixin:
     def remove_unconfident_sentiment(self, df):
@@ -37,7 +40,26 @@ class PreprocessingMixin:
         return converted_df
 
 
+    def remove_stopwords(self, input_text):
+        stopwords_list = stopwords.words('english')
+        # Some words which might indicate a certain sentiment are kept via a whitelist
+        whitelist = ["n't", "not", "no"]
+        words = input_text.split() 
+        clean_words = [word for word in words if (word not in stopwords_list or word in whitelist) and len(word) > 1] 
+        return " ".join(clean_words)
+
+
+
     def preprocessing(self, df):
         confident_df = self.remove_unconfident_sentiment(df)
         web_cleaned_df = self.remove_html_and_tweet_tags(confident_df)
+        web_cleaned_df.text_final = web_cleaned_df.text_final.apply(remove_stopwords)
         return self.convert_sentiment_to_scalar(web_cleaned_df)
+    
+    def get_train_test(self, df):
+        train, test = train_test_split(df, test_size=0.2, random_state=1)
+        X_train = train['text_final'].values
+        X_test = test['text_final'].values
+        y_train = train['sentiment']
+        y_test = test['sentiment']
+        return X_train, X_test, y_train, y_test
